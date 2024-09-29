1cd829ad53687de80e085606fb9a5f091ac4e472
Emily Middleton <emily.a.middleton@gmail.com>
2024-09-29 08:24:56 -0400

    wrote passenger class, have not checked it

--- Passenger.java
+++ Passenger.java
@@ -5,4 +5,27 @@ public class Passenger {
     public Passenger(String name) {
         this.name = name;
     }
+
+    //Accessor
+    public String getName(){
+        return name;
+    }
+
+    //if there is space in a car, the passenger may enter
+    public boolean boardCar(Car c){
+        if (c.addPassenger(this)){ //checks if addPassenger returns true
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public boolean getOffCar(Car c){
+        if (c.removePassenger(this)){ //if they were on the car, they can be removed
+            return true;
+        } else{
+            return false;
+        }
+    }
+
 }
