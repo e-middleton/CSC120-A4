cce12bc2f01678865bad8eded9417d34d62e83b9
Emily Middleton <emily.a.middleton@gmail.com>
2024-09-30 09:01:29 -0400

    wrote train class, needs more testing

--- Train.java
+++ Train.java
@@ -1,3 +1,89 @@
-public class Train {
+import java.util.ArrayList;
 
+//how do I initialize cars from my train class/use the passenger capacity int?
+/**
+ * class Train which has an Arraylist of Cars that belong to the train as well as an Engine that it uses to run
+ * has methods for accessing the attributes as well as getting the maximum capacity, seats remaining, and a printed
+ * list of who is on the entire train
+ */
+public class Train {
+    private ArrayList<Car> Cars; //the train of cars
+    private Engine trainEngine;
+
+    public Train(FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity){
+        this.trainEngine = new Engine(fuelType, fuelCapacity);
+        this.Cars = new ArrayList<Car>(nCars); 
+        for(int i = 0; i < nCars; i++){
+            Car boxCar = new Car(passengerCapacity);
+            Cars.add(boxCar); //initializes car objects inside the arrayList
+        }
+    }
+
+    /**
+     * Accessor for the Engine
+     * @return the Engine for this specific train
+     */
+    public Engine getEngine(){
+        return trainEngine;
+    }
+
+    /**
+     * finds a car at a specified index in the ArrayList of all the cars in the train
+     * @param i the index for the ArrayList of cars, or the number of the train car you want to find
+     * @returnt the Car at the index i
+     */
+    public Car getCar(int i){
+        return Cars.get(i);
+    }
+
+    /**
+     * finds the maximum capacity of the entire train by going into each car and adding its maximum capacity
+     * @return the maximum capacity of the train
+     */
+    public int getMaxCapacity(){
+        return Cars.size() * (Cars.get(0).getMaxCapacity()); 
+    }
+
+    /**
+     * finds how many seats are empty across the entire train by going to each car and getting its empty seats
+     * then adding it to the other empty seats
+     * @return the number of seats empty
+     */
+    public int seatsRemaining(){
+        int seats = 0;
+        if(Cars.size() > 0){
+            for(int i = 0; i < Cars.size(); i ++){
+                seats += Cars.get(i).seatsRemaining();
+            }
+        } 
+        return seats;
+    }
+
+    /**
+     * Function that prints out all the passengers in the train by going to each car 
+     * (if it exists) and asking the car to print its passengers
+     */
+    public void printManifest(){
+        if(Cars.size() > 0){
+            for(int i = 0; i < Cars.size(); i++){
+                Cars.get(i).printManifest();
+            }
+        } else{
+            System.out.println("Whoops, empty train? :()");
+        }
+    }
+
+public static void main(String[] args) {
+    Train myTrain = new Train(FuelType.ELECTRIC, 100, 3, 25);
+        Passenger victim = new Passenger("Jane");
+        Passenger victim2 = new Passenger("Tim");
+        //System.out.println(myTrain.seatsRemaining());
+        myTrain.getCar(0).addPassenger(victim);
+        myTrain.getCar(1).addPassenger(victim2);
+        myTrain.printManifest();
+        System.out.println();
+
+        victim.getOffCar(myTrain.getCar(0));
+        myTrain.printManifest();
+    }
 }
